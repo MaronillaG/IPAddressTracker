2eb1c51344f7477b62b04ffc2df36ee9e79669a6
Gillian <105639251+MaronillaG@users.noreply.github.com>
2023-07-05 11:57:20 +0200

    adding map and function for generating user IP on first load.

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const showtimezone = document.querySelector('#timezone-input');
 const showisp = document.querySelector('#isp-input');
 const searchInput = document.querySelector('#search-Input');
 
+let lat = 0;
+let long = 0;
+
+
 function submitInput(event) {
     event.preventDefault();
     
@@ -31,11 +35,15 @@ async function getDetails(IPDetailsAPI) {
     showLocation.innerText = `${region}, ${city} ${postcode}`;
     showtimezone.innerText = `UTC ${time}`;
     showisp.innerText = isp;
+
+    lat = location.lat;
+    long = location.lng;
     
     console.log('show IP ',ip);
     console.log('timezone', location.timezone);
     console.log('timezone check var', time);
     console.log('service provider', isp);
+    console.log('lat + long', lat, long);
 }
 
 
@@ -49,3 +57,28 @@ function resetVals() {
 }
 
 resetVals();
+
+let coordinates = [lat, long]
+const map = L.map('map').setView(coordinates, 14);
+const attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+const tileUrl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, {attribution} );
+
+tiles.addTo(map);
+
+const marker = L.marker(coordinates).addTo(map);
+
+
+
+
+// function that requests the user's IPv4 address to be used with map info
+async function firstIP() {
+    const response = await fetch('https://api.ipify.org?format=json');
+    const data = await response.json();
+    console.log('user\'s IP on first load', data.ip);
+    return data.ip
+}
+
+
+    
+// console.log(default);
